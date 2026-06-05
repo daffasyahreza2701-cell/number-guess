a87e582596c14d2101697b29426db89a948c2cb0
Daffa Syahreza <daffasyahreza2701@gmail.com>
2026-06-05 10:56:13 -0400

    feat: username not found

--- number_guess.sh
+++ number_guess.sh
@@ -6,3 +6,13 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo "Enter your username:"
 read USERNAME
 USERNAME_EXIST=$($PSQL "SELECT username from users WHERE username = '$USERNAME'")
+#if username is not found
+if [[ -z $USERNAME_EXIST ]]
+  then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_UNAME=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  else
+  #if username is found
+  VIEW_USER=$($PSQL "SELECT username, COUNT(*), MIN(guess) FROM users LEFT JOIN score USING(user_id)")
+  IFS='|' read USER PLAY GUESS <<< "$VIEW_USER"
+  echo "Welcome back, $USER! You have played $PLAY games, and your best game took $GUESS guesses."
